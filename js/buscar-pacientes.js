877f248a6e25cf5f9c4f98321170c94af8852ebe
Teteuzins2 <mateuspvz1@gmail.com>
2022-07-19 23:52:48 -0300

    upload

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -10,7 +10,25 @@ botaoAdicionar.addEventListener("click", function(){
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
     xhr.addEventListener("load", function(){
-        console.log(xhr.responseText);
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if( xhr.status == 200){
+            erroAjax.classList.add("invisivel");
+            var responseText;
+            var pacientes = JSON.parse(resposta);
+            
+            pacientes.forEach( function(paciente){
+                adicionaPacienteNaTabela(paciente);
+            })
+
+        }else{
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            
+            erroAjax.classList.remove("invisivel");
+        
+        }
+    
     })
 
     xhr.send();
